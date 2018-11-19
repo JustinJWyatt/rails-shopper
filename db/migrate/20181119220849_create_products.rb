7f1a3c51ee6eb1c4e622393bcbf9162ef4a408d0
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-19 16:40:30 -0600

    created database and added migrations

--- db/migrate/20181119220849_create_products.rb
+++ db/migrate/20181119220849_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
-      t.string :title
+      t.string :title, null: false #not nullable
       t.text :description
-      t.decmial :price
+      t.decimal :price, null: false, precision: 15, scale: 2 #width of range to allow for price and not nullable
 
       t.timestamps
     end
